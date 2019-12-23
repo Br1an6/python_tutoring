5e1e0c91c172edb25a9aaeead6207d770bdabd09
Liu <yuanan@a483e72c190f.ant.amazon.com>
2019-12-23 10:36:03 -0800

    add a 1

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # python_tutoring
+https://docs.python.org/zh-tw/3/tutorial/index.html
---
+++ a1/a1_sol.py
@@ -0,0 +1,56 @@
+# A-1 learn basics and conditions
+import math
+
+def getSideC():
+    """
+       /|
+    x / | 4
+     /__|
+      3
+
+    x = math.sqrt(9+16)
+    >>> getSideC()
+    5
+    """
+    side_a = 3
+    side_b = 4
+    side_c = 0
+    # TODO: print of the value for side c
+    side_c = math.sqrt(side_a ** 2 + side_b ** 2)
+    print "Side c is:", side_c
+
+
+def printPosOrNeg(num):
+    """
+    look at the print above. Use if and else.
+    print "is positive"
+    print "is negative"
+    """
+    #pass # remove me
+    if num >= 0:
+    	print "is positive"
+    else:
+    	print "is negative"
+
+
+
+def main():
+    """ Main method"""
+    print "===This is A-1==="
+    print "=======Your output======"
+    print "[*] Part 1 getSideC output: "
+    getSideC() # should print 5
+    print "\n"
+    print "[*] Part 2 printPosOrNeg output: "
+    for num in [1, -1, 2, 4]:
+        print "input ", num, " is :"
+        printPosOrNeg(num)
+    print "==========Done=========="
+    print "\n\n"
+    print "==========Answer========"
+    print "p1: 5  p2: is positive, is negative, is positive, is positive"
+
+
+if __name__ == '__main__':
+    main()
+
